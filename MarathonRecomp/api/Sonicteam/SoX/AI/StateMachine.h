8ddf56064eca815e714096f1eaf3c58344c68fd6
Isaac Marovitz <isaacryu@icloud.com>
2025-09-04 17:03:20 -0400

    Fix build
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- MarathonRecomp/api/Sonicteam/SoX/AI/StateMachine.h
+++ MarathonRecomp/api/Sonicteam/SoX/AI/StateMachine.h
@@ -11,11 +11,11 @@ namespace Sonicteam::SoX::AI
         xpointer<void> m_pVftable;
         xpointer<StateMachine<TState>> m_pState;
         MARATHON_INSERT_PADDING(4);
-        xpointer<IContext> m_pContext;
+        xpointer<Player::State::IContext> m_pContext;
         MARATHON_INSERT_PADDING(4);
         be<float> m_Time;
 
-        template <typename TContext = IContext>
+        template <typename TContext = Player::State::IContext>
         TContext* GetContext()
         {
             return (TContext*)m_pContext.get();
