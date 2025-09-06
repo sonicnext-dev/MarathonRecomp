f846f464393b827a3d7c3053523f9bf9ff8d4dff
Isaac Marovitz <isaacryu@icloud.com>
2025-09-05 23:41:27 -0400

    RestoreChainJumpFlips
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- MarathonRecomp/user/config_def.h
+++ MarathonRecomp/user/config_def.h
@@ -83,6 +83,7 @@ CONFIG_DEFINE_HIDDEN("Codes", bool, FixPowerUpJingleDuration, false);
 CONFIG_DEFINE_HIDDEN("Codes", bool, UseOfficialTitleOnTitleBar, false);
 CONFIG_DEFINE_HIDDEN("Codes", bool, DisableLowResolutionFontOnCustomUI, false);
 CONFIG_DEFINE_HIDDEN("Codes", bool, RestoreContextualHUDColours, false);
+CONFIG_DEFINE_HIDDEN("Codes", bool, RestoreChainJumpFlips, false);
 CONFIG_DEFINE_HIDDEN("Codes", bool, DisableEdgeGrabLeftover, false);
 CONFIG_DEFINE_HIDDEN("Codes", bool, TailsGauge, false);
 CONFIG_DEFINE_HIDDEN("Codes", bool, PlayerDebugMode, false);
