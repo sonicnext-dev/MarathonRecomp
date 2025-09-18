275c19cd1a92225ea152efc768ba625e189e53cc
Isaac Marovitz <isaacryu@icloud.com>
2025-09-18 12:55:32 -0400

    Improve API
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- MarathonRecomp/api/Sonicteam/HUDPause.h
+++ MarathonRecomp/api/Sonicteam/HUDPause.h
@@ -5,10 +5,11 @@
 
 namespace Sonicteam
 {
-    class HUDPause : public SoX::Engine::Task
+    class HUDPause : public SoX::RefCountObject, public SoX::Engine::Task
     {
     public:
-        MARATHON_INSERT_PADDING(0xAC);
+        xpointer<CsdObject> m_pCsdObject;
+        MARATHON_INSERT_PADDING(0xA0);
         be<float> m_TextPriority;
         MARATHON_INSERT_PADDING(0x44);
         bool m_ShowMissionWindow;
