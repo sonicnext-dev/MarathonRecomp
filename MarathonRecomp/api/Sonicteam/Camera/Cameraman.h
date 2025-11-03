d61373c733b1519395e6c36beec6c36faf39570f
Hyper <34012267+hyperbx@users.noreply.github.com>
2025-11-03 12:44:04 +0000

    Sonicteam::Camera offset assertions

--- MarathonRecomp/api/Sonicteam/Camera/Cameraman.h
+++ MarathonRecomp/api/Sonicteam/Camera/Cameraman.h
@@ -14,5 +14,10 @@ namespace Sonicteam::Camera
         boost::shared_ptr<CameraModeManager> m_spCameraModeManager;
         MARATHON_INSERT_PADDING(0x2C);
         be<float> m_FOV;
+        MARATHON_INSERT_PADDING(0x0C);
     };
+
+    MARATHON_ASSERT_OFFSETOF(Cameraman, m_spCameraModeManager, 0x7C);
+    MARATHON_ASSERT_OFFSETOF(Cameraman, m_FOV, 0xB0);
+    MARATHON_ASSERT_SIZEOF(Cameraman, 0xC0);
 }
--- MarathonRecomp/api/Sonicteam/Camera/SonicCamera.h
+++ MarathonRecomp/api/Sonicteam/Camera/SonicCamera.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Marathon.inl>
+
 namespace Sonicteam::Camera
 {
     class SonicCamera : public CameraMode
@@ -22,4 +24,17 @@ namespace Sonicteam::Camera
         be<float> m_Distance;
         MARATHON_INSERT_PADDING(0x0C);
     };
+
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_pDoc, 0x2C);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_SpringK, 0x44);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_DampingK, 0x48);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_AzDamping, 0x4C);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_AltDamping, 0x50);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_AzDriveK, 0x54);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_AzDampingK, 0x58);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_AltDriveK, 0x5C);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_AltDampingK, 0x60);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_FovY, 0xA0);
+    MARATHON_ASSERT_OFFSETOF(SonicCamera, m_Distance, 0xE0);
+    MARATHON_ASSERT_SIZEOF(SonicCamera, 0xF0);
 }
