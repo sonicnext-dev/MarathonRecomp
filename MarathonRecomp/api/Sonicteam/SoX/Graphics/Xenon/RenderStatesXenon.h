251c266e97ebaae080373062e055e1e888c128b3
Isaac Marovitz <isaacryu@icloud.com>
2025-11-04 16:28:36 -0500

    Add stuff to Graphics::Device
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- MarathonRecomp/api/Sonicteam/SoX/Graphics/Xenon/RenderStatesXenon.h
+++ MarathonRecomp/api/Sonicteam/SoX/Graphics/Xenon/RenderStatesXenon.h
@@ -3,7 +3,7 @@
 #include <Marathon.inl>
 #include <Sonicteam/SoX/Graphics/RenderStates.h>
 
-namespace Sonicteam::SoX::Graphics
+namespace Sonicteam::SoX::Graphics::Xenon
 {
     class RenderStatesXenon : public RenderStates
     {
