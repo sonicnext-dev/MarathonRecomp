111869b57262e57f93c8661260659d487017c1f4
Isaac Marovitz <isaacryu@icloud.com>
2025-11-08 15:46:31 -0500

    Fix MyRenderProcess mistake
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- MarathonRecomp/api/Sonicteam/MyRenderProcess.h
+++ MarathonRecomp/api/Sonicteam/MyRenderProcess.h
@@ -9,11 +9,11 @@ namespace Sonicteam
     class MyRenderProcess : public SoX::Engine::RenderProcess
     {
     public:
-        xpointer<RenderProcess> m_pParent;
+        xpointer<DocMarathonState> m_pDocMarathonState;
         xpointer<MyGraphicsDevice> m_pMyGraphicsDevice;
     };
 
     MARATHON_ASSERT_SIZEOF(MyRenderProcess, 0x38);
-    MARATHON_ASSERT_OFFSETOF(MyRenderProcess, m_pParent, 0x30);
+    MARATHON_ASSERT_OFFSETOF(MyRenderProcess, m_pDocMarathonState, 0x30);
     MARATHON_ASSERT_OFFSETOF(MyRenderProcess, m_pMyGraphicsDevice, 0x34);
 }
