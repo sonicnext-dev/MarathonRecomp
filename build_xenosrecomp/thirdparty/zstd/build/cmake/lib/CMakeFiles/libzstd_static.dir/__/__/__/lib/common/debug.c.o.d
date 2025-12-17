12220459074d98fb6921e67f39b68be699636f93
Prince Ozordi <ozordip@gmail.com>
2025-12-17 01:40:47 -0400

    chore: Remove tracked junk files and update .gitignore
    - Remove shader_batch/, shader_batch2/, shader_batch3/, shader_batch4/ (copyrighted game assets)
    - Remove test_shaders/ (copyrighted game assets)
    - Remove build_xenosrecomp/ (build output)
    - Add comprehensive .gitignore patterns for:
      - Development scripts (run*.sh, run_xenon.py)
      - Temp output files (log2.txt, recomp_output.txt, etc.)
      - Shader batches and extracted shaders
      - Build tool output directories

--- build_xenosrecomp/thirdparty/zstd/build/cmake/lib/CMakeFiles/libzstd_static.dir/__/__/__/lib/common/debug.c.o.d
+++
@@ -1,3 +0,0 @@
-thirdparty/zstd/build/cmake/lib/CMakeFiles/libzstd_static.dir/__/__/__/lib/common/debug.c.o: \
-  /Users/Ozordi/Downloads/MarathonRecomp/tools/XenosRecomp/thirdparty/zstd/lib/common/debug.c \
-  /Users/Ozordi/Downloads/MarathonRecomp/tools/XenosRecomp/thirdparty/zstd/lib/common/debug.h
